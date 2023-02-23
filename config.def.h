b09de60b9e65fd7ec6f2ebc6847c6b16a3da6919
marco-04 <61947904+marco-04@users.noreply.github.com>
2023-02-23 16:44:38 +0100

    Revert xresources-alt patch

--- config.def.h
+++ config.def.h
@@ -7,9 +7,7 @@ static int constout = 0;                    /* -o  option; if 1, outputs current
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-  font,
 	"monospace:size=10"
 };
 
@@ -22,32 +20,22 @@ static int use_prefix = 1;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static char normfgcolor[] = "#bbbbbb";
-static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
-static char midfgcolor[]  = "#eeeeee";
-static char midbgcolor[]  = "#770000";
-static char normfgcolorh[]= "#ffc987";
-static char normbgcolorh[]= "#222222";
-static char selfgcolorh[] = "#ffc987";
-static char selbgcolorh[] = "#005577";
-static char *colors[SchemeLast][2] = {
-	/*                        fg            bg       */
-	[SchemeNorm]          = { normfgcolor,  normbgcolor  },
-	[SchemeSel]           = { selfgcolor,   selbgcolor   },
-	[SchemeMid]           = { midfgcolor,   midbgcolor   },
-	[SchemeOut]           = { "#000000",    "#00ffff"    },
-	[SchemeNormHighlight] = { normfgcolorh, normbgcolorh },
-	[SchemeSelHighlight]  = { selfgcolorh,  selbgcolorh  },
-	[SchemeOutHighlight]  = { "#ffc978",    "#00ffff"    },
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*                         fg         bg       */
+	[SchemeNorm]          = { "#bbbbbb", "#222222" },
+	[SchemeSel]           = { "#eeeeee", "#005577" },
+	[SchemeMid]           = { "#eeeeee", "#770000" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
+	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOutHighlight]  = { "#ffc978", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel]  = { OPAQUE, alpha },
-	[SchemeOut]  = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -75,21 +63,3 @@ static int fullwidth = 0;
 
 /* -n option; preselected item starting from 0 */
 static unsigned int preselected = 0;
-
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-	{ "font",         STRING, &font },
-	{ "normfgcolor",  STRING, &normfgcolor  },
-	{ "normbgcolor",  STRING, &normbgcolor  },
-	{ "selfgcolor",   STRING, &selfgcolor   },
-	{ "selbgcolor",   STRING, &selbgcolor   },
-	{ "midfgcolor",   STRING, &midfgcolor   },
-	{ "midbgcolor",   STRING, &midbgcolor   },
-	{ "normfgcolorh", STRING, &normfgcolorh },
-	{ "normbgcolorh", STRING, &normbgcolorh },
-	{ "selfgcolorh",  STRING, &selfgcolorh  },
-	{ "selbgcolorh",  STRING, &selbgcolorh  },
-	{ "prompt",       STRING, &prompt       },
-};
