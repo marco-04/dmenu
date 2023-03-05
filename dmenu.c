88add254e932fa76024ce5228bba4925c269fa21
marco-04 <61947904+marco-04@users.noreply.github.com>
2023-03-05 15:36:51 +0100

    Fixed border patch

--- dmenu.c
+++ dmenu.c
@@ -1093,7 +1093,7 @@ setup(void)
 	swa.border_pixel = 0;
 	swa.colormap = cmap;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
-	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, border_width,
+  win = XCreateWindow(dpy, parentwin, x, y - (topbar ? 0 : border_width * 2), mw - border_width * 2, mh, border_width,
 	                    depth, CopyFromParent, visual,
 	                    CWOverrideRedirect | CWBackPixel | CWBorderPixel | CWColormap | CWEventMask, &swa);
 	if (border_width)
